c77fa52abd8c88afa70e215bab3b18854b2ff515
OlenaKut <cundrochka@gmail.com>
2023-01-31 13:31:52 +0100

    Added socialmedia links

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import Logo from "../pictures/Logo.png";
 import "./HeaderMenu.css";
 import Button from "react-bootstrap/Button";
 import Menu from "../components/Menu";
+import MediaLinks from "../components/MediaLinks";
 
 const Header = () => {
   return (
@@ -10,10 +11,12 @@ const Header = () => {
       <div>
         {" "}
         <img src={Logo} alt="Logo" className="logo" />
-        <Button className="donate">
-          BECOME A VOLONTEER
-        </Button>
+        <Button className="donate">BECOME A VOLONTEER</Button>
         <Button className="donate">DONATE HERE</Button>
+      </div>
+      <div className="d-none d-sm-block">
+        {" "}
+        <MediaLinks />
       </div>
       <div className="position-absolute end-0">
         <Menu />
